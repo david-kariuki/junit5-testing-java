de2f4b6bc0ebe5e6c13699f4f91b483e4d8ba75d
dk <workplace.davidkariuki@gmail.com>
2022-07-12 11:22:48 +0300

    First test class with fail() assertion

---
+++ src/test/java/FirstJunit5Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FirstJunit5Test {
+
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
