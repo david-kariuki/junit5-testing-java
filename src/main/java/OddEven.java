588f2fd9e2e67aca594d1084f1ccd3cafbc7bf83
dk <workplace.davidkariuki@gmail.com>
2022-07-12 11:33:53 +0300

    Wrote test cases for even number checker method

---
+++ src/main/java/OddEven.java
@@ -0,0 +1,18 @@
+/**
+ * OddEven class
+ *
+ * @author David Kariuki
+ * @since 12/7/2022
+ */
+public class OddEven {
+
+	/**
+	 * Method to check if number is odd or even
+	 *
+	 * @param number - int
+	 * @return boolean
+	 */
+	public boolean isNumberEven(int number) {
+		return number % 2 == 0;
+	}
+}
---
+++ src/test/java/OddEvenTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OddEvenTest {
+
+	/**
+	 * Test method to test for true if even is passed to the isEven method
+	 */
+	@Test
+	void givenEvenNumber_whenIsEvenIsCalled_returnTrue() {
+		OddEven oddEven = new OddEven();
+		assertTrue(oddEven.isNumberEven(10));
+	}
+
+	/**
+	 * Test method to test for false if even is passed to the isEven method
+	 */
+	@Test
+	void  givenOddNumber_whenIsEvenIsCalled_returnedFalseIs() {
+		OddEven oddEven = new OddEven();
+		assertFalse(oddEven.isNumberEven(13));
+	}
+}
