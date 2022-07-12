1d3616d48959e3717311b4bb8bbff0990b7a7231
David Kariuki <dkaris.k@gmail.com>
2022-07-12 22:58:40 +0300

    Wrote test codes for The assertFalse assertion

---
+++ src/test/java/AssertionFalseStringUtilsTest.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * AssertionTrueStringUtilsTest class
+ *
+ * @author David Kariuki
+ * @apiNote - This test class contains test methods for the StringUtils.java class
+ * @since 12/7/2022
+ */
+
+@SuppressWarnings("ConstantConditions")
+public class AssertionFalseStringUtilsTest {
+
+    /**
+     * Method to test assert false with false condition
+     */
+    @Test
+    public void testAssertFalseWithFalseCondition() {
+        boolean falseValue = false;
+        assertFalse(falseValue);
+    }
+
+    /**
+     * Method to test assert false with true condition
+     */
+    @Test
+    public void testAssertFalseWithTrueCondition() {
+        boolean trueValue = true;
+        assertFalse(trueValue);
+    }
+
+    /**
+     * Method to test assert false with true condition and message
+     */
+    @Test
+    public void testAssertFalseWithTrueConditionAndMessage() {
+        boolean trueValue = true;
+        assertFalse(trueValue, "The actual value is true");
+    }
+
+    /**
+     * Method to test assert false with true condition and message supplier
+     */
+    @Test
+    public void testAssertFalseWithTrueConditionAndMessageSupplier() {
+        boolean trueValue = true;
+        assertFalse(trueValue, () -> "The actual value is true");
+    }
+
+    /**
+     * Method to test assert false with boolean supplier
+     */
+    @Test
+    public void testAssertFalseWithBooleanSupplier() {
+        boolean falseValue = false;
+        assertFalse(() -> falseValue);
+    }
+
+    /**
+     * Method to test assert false with boolean supplier and message
+     */
+    @Test
+    public void testAssertFalseWithBooleanSupplierAndMessage() {
+        boolean trueValue = true;
+        assertFalse(() -> trueValue, "The actual value is true");
+    }
+
+    /**
+     * Method to test assert false with boolean supplier and message supplier. condition
+     */
+    @Test
+    public void testAssertFalseWithBooleanSupplierAndMessageSupplier() {
+        boolean trueValue = true;
+        assertFalse(() -> trueValue, () -> "The actual value is true");
+    }
+}
