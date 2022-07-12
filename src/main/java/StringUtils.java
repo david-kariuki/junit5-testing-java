dbf1cc15e43a02c720a5d1dfa770a5c51ce89645
dk <workplace.davidkariuki@gmail.com>
2022-07-12 12:19:57 +0300

    Wrote tests for the assertNotNull JUnit5 assertion

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,43 @@
+/**
+ * StringUtils class
+ *
+ * @author David Kariuki
+ * @apiNote - This class contains a string reverse method
+ * @since 12/7/2022
+ */
+public class StringUtils {
+
+	/**
+	 * Method to reverse passed string
+	 *
+	 * @param input - String
+	 * @return String
+	 */
+	public static String reverse(String input) {
+
+		// Check for null
+		if (input == null) {
+			return null;
+		}
+
+		// Check string length
+		if (input.length() == 0) {
+			return "";
+		}
+
+		char[] charArray = input.toCharArray();
+		int start = 0;
+		int end = charArray.length - 1;
+
+		// Loop while reversing String
+		while (start < end) {
+			char temp = charArray[start];
+			charArray[start] = charArray[end];
+			charArray[end] = temp;
+			start++;
+			end--;
+		}
+
+		return new String(charArray); // Return reversed string
+	}
+}
---
+++ src/test/java/StringUtilsTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * StringUtilsTest class
+ *
+ * @author David Kariuki
+ * @apiNote - This test class contains test methods for the StringUtils.java class
+ * @since 12/7/2022
+ */
+
+@SuppressWarnings("ConstantConditions")
+public class StringUtilsTest {
+
+
+	/**
+	 * Test method to test for null with assertNull after passing null
+	 */
+	@Test
+	void givenNullString_NullReturned() {
+		String actual = StringUtils.reverse(null);
+
+		assertNull(actual); // Assert without message
+	}
+
+	/**
+	 * Test method to test for null with assertNull with message after passing empty string ("")
+	 */
+	@Test
+	void givenEmptyString_EmptyStringReturnedWithMessage() {
+		String actual = StringUtils.reverse("");
+
+		assertNull(actual, "The actual string should be null!!"); // Assert without message
+	}
+
+	/**
+	 * Test method to test for null with assertNull with message after passing String to reverse
+	 */
+	@Test
+	void givenNonNullString_ReversedStringReturned() {
+		String actual = StringUtils.reverse("ABCD");
+		Supplier<String> messageSupplier = () -> "The actual string should be null!!";
+
+		assertNull(actual, messageSupplier); // Assert with java 8 MessageSupplier
+	}
+}
