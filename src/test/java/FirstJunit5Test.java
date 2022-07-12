9939a551651edc9b81f4f8369da350201014f3c1
dk <workplace.davidkariuki@gmail.com>
2022-07-12 11:45:47 +0300

    Updated documentation

--- src/test/java/FirstJunit5Test.java
+++ src/test/java/FirstJunit5Test.java
@@ -1,3 +1,11 @@
+/**
+ * FirstJunit5Test class
+ *
+ * @author David Kariuki
+ * @apiNote - This test class contains test methods
+ * @since 12/7/2022
+ */
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -5,6 +13,9 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class FirstJunit5Test {
 
 
+	/**
+	 * Test method with fail assertion
+	 */
 	@Test
 	void test() {
 		fail("Not yet implemented");
