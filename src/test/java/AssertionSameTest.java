7729e5f8ed7f44a028d5caf1a2fb0b3955e51797
David Kariuki <dkaris.k@gmail.com>
2022-07-14 00:09:58 +0300

    Wrote test codes for The assertSame assertion

---
+++ src/test/java/AssertionSameTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * AssertionSameTest class
+ *
+ * @author David Kariuki
+ * @apiNote - This test class contains test methods for JUnit Assertion - Same
+ * @since 12/7/2022
+ */
+
+@SuppressWarnings("ExcessiveLambdaUsage")
+public class AssertionSameTest {
+
+    /**
+     * Method to test assertion same with same object
+     */
+    @Test
+    public void testAssertSameWithSameObject() {
+        String actual = "hello";
+        String expected = "hello";
+        assertSame(expected, actual);
+    }
+
+    /**
+     * Method to test assertion same with different object
+     */
+    @Test
+    public void testAssertSameWithDifferentObject() {
+        String actual = "hello";
+        String expected = "hello there";
+        assertSame(expected, actual);
+    }
+
+    /**
+     * Method to test assertion same with different object and message
+     */
+    @Test
+    public void testAssertSameWithDifferentObjectAndMessage() {
+        String actual = "hello";
+        String expected = "hello there";
+        assertSame(expected, actual, "The actual value is not same as expected value");
+    }
+
+    /**
+     * Method to test assertion same with different object and message supplier
+     */
+    @Test
+    public void testAssertSameWithDifferentObjectAndMessageSupplier() {
+        String actual = "hello";
+        String expected = "hello there";
+        assertSame(expected, actual, () -> "The actual value is not same as expected value");
+    }
+}
