d2f35dcee307c23f63c43c019a0750d20b96c82c
David Kariuki <dkaris.k@gmail.com>
2022-08-07 16:41:27 +0300

    Operating System Conditions - @DisabledOnOs and @EnabledOnOs

---
+++ src/test/java/AnnotationEnabledDisabledOnOSTest.java
@@ -0,0 +1,50 @@
+import jdk.jfr.Enabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * AnnotationEnabledDisabledOnOSTest class
+ *
+ * @author David Kariuki
+ * @apiNote - This test class contains test methods for the EnabledOnOs and @DisabledOnOs
+ * @since 7/8/2022
+ */
+
+@SuppressWarnings("ConstantConditions")
+@Enabled
+public class AnnotationEnabledDisabledOnOSTest {
+
+    @Test
+    void testOnAllOs() {
+        assertTrue(3 > 0);
+    }
+
+    @DisabledOnOs(OS.MAC)
+    @Test
+    void testDisableOnMacOs() {
+        assertFalse(0 > 4);
+    }
+
+    @DisabledOnOs(OS.WINDOWS)
+    @Test
+    void testDisableOnWindowOs() {
+        assertFalse(10 > 40);
+    }
+
+    @EnabledOnOs(OS.LINUX)
+    @Test
+    void testEnableOnLinuxOs() {
+        assertFalse(10 > 50);
+    }
+
+    @EnabledOnOs(OS.SOLARIS)
+    @Test
+    void testEnableOnSolarisOs() {
+        assertFalse(15 > 40);
+    }
+}
