6e5fc2000fd08acd1a7d85dd32edc2ae1189fb77
David Kariuki <dkaris.k@gmail.com>
2022-08-07 14:13:56 +0300

    @BeforeAll, @AfterAll and @DisplayName annotations

---
+++ src/test/java/AnnotationBeforeAllAndAfterAll.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.*;
+
+/**
+ * AssertEqualsTest class
+ *
+ * @author David Kariuki
+ * @apiNote - This test class contains test methods for the assertions @beforeAll and @AfterAll
+ * @since 12/7/2022
+ */
+
+@SuppressWarnings("NewClassNamingConvention")
+@DisplayName("Annotations @BeforeAll and @AfterAll")
+public class AnnotationBeforeAllAndAfterAll {
+
+    @DisplayName("@BeforeAll Test Method")
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("LifecycleTest - beforeAll() got executed !!!");
+    }
+
+    public AnnotationBeforeAllAndAfterAll() {
+        System.out.println("LifecycleTest - Constructor got executed !!!");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("LifecycleTest - beforeEach() got executed !!!");
+    }
+
+    @Test
+    public void testOne() {
+        System.out.println("LifecycleTest - testOne() got executed !!!");
+    }
+
+    @Test
+    public void testTwo() {
+        System.out.println("LifecycleTest - testTwo() got executed !!!");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("LifecycleTest - afterEach() got executed !!!");
+    }
+
+    @DisplayName("@AfterAll Test Method")
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("LifecycleTest - afterAll() got executed !!!");
+    }
+}
