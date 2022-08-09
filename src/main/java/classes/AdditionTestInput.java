6185476c9b81b8512368288e2060bb0c86c5e975
David Kariuki <dkaris.k@gmail.com>
2022-08-10 00:34:12 +0300

    ParameterResolverExtension

---
+++ src/main/java/classes/AdditionTestInput.java
@@ -0,0 +1,14 @@
+package classes;
+
+import lombok.*;
+
+@SuppressWarnings("ClassCanBeRecord")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+public class AdditionTestInput {
+
+    private final int input1;
+    private final int input2;
+}
---
+++ src/test/java/extensions/ParameterResolverExtensionTest.java
@@ -0,0 +1,32 @@
+/**
+ * Test class for ParameterResolverExtension class
+ *
+ * @author David Kariuki
+ * @see extensions.ParameterResolverExtension
+ * @since 12/8/2022
+ */
+package extensions;
+
+import classes.AdditionTestInput;
+import classes.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(ParameterResolverExtension.class)
+public class ParameterResolverExtensionTest {
+
+    private static Calculator calculator;
+
+    @BeforeAll
+    public static void initialize() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void firstTest(AdditionTestInput additionTestInput) {
+        Assertions.assertEquals(3,
+                calculator.add(additionTestInput.getInput1(), additionTestInput.getInput2()));
+    }
+}
